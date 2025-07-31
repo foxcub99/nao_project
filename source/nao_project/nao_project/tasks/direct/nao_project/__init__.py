7b5d3786ffad05036403726152e4e0dad25cc798
Reilly <reillyfox6@gmail.com>
2025-07-31 15:11:54 +1000

    removed unused things that were clogging me up. Can re-duplicate later if needed, now that its kinda working

--- source/nao_project/nao_project/tasks/direct/nao_project/__init__.py
+++ source/nao_project/nao_project/tasks/direct/nao_project/__init__.py
@@ -8,8 +8,6 @@ import gymnasium as gym
 from . import agents
 
 from .nao_env import NaoEnv, NaoEnvCfg
-from .nao_envb import NaoEnvb, NaoEnvbCfg
-from .nao_envc import NaoEnvc, NaoEnvcCfg
 
 ##
 # Register Gym environments.
@@ -20,28 +18,6 @@ gym.register(
     disable_env_checker=True,
     kwargs={
         "env_cfg_entry_point": NaoEnvCfg,
-        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
-        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:HumanoidPPORunnerCfg",
-        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
-    },
-)
-gym.register(
-    id="Isaac-Nao-Direct-v0",
-    entry_point="isaaclab_tasks.direct.nao:NaoEnvb",
-    disable_env_checker=True,
-    kwargs={
-        "env_cfg_entry_point": NaoEnvbCfg,
-        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
-        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:HumanoidPPORunnerCfg",
-        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
-    },
-)
-gym.register(
-    id="Isaac-Nao-Direct-v1",
-    entry_point="isaaclab_tasks.direct.nao:NaoEnvc",
-    disable_env_checker=True,
-    kwargs={
-        "env_cfg_entry_point": NaoEnvcCfg,
         "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:HumanoidPPORunnerCfg",
         "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
